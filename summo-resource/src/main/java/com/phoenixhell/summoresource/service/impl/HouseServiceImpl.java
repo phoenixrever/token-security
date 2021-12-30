b6b7bb1780eb36baa4a57317c327ccea211d9779
phoenixhell <phoenixrever@gmail.com>
2021-12-30 21:46:27 +0800

    2021-12-30 true power!!!

--- summo-resource/src/main/java/com/phoenixhell/summoresource/service/impl/HouseServiceImpl.java
+++ summo-resource/src/main/java/com/phoenixhell/summoresource/service/impl/HouseServiceImpl.java
@@ -9,6 +9,8 @@ import com.phoenixhell.summoresource.service.NeighborhoodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -22,6 +24,7 @@ import com.phoenixhell.common.utils.Query;
 import com.phoenixhell.summoresource.mapper.HouseMapper;
 import com.phoenixhell.summoresource.entity.HouseEntity;
 import com.phoenixhell.summoresource.service.HouseService;
+import org.springframework.util.StringUtils;
 
 
 @Service("houseService")
@@ -67,6 +70,10 @@ public class HouseServiceImpl extends ServiceImpl<HouseMapper, HouseEntity> impl
         int pageSize=10;
         int pageNo=1;
         String order="price";
+        String giftPrice=null;
+        String deposit=null;
+        List<String> classify=null;
+        List<String> buildMaterial=null;
         if(params.get("pageSize")!=null){
              pageSize = Integer.parseInt((String) params.get("pageSize"));
         }
@@ -76,9 +83,25 @@ public class HouseServiceImpl extends ServiceImpl<HouseMapper, HouseEntity> impl
         if(params.get("order")!=null){
             order = (String) params.get("order");
         }
-        List<HouseEntity> houseEntities =  baseMapper.getOrderPage(order,pageSize,pageNo);
+
+        if(!StringUtils.isEmpty(params.get("classify"))){
+            classify = Arrays.asList(params.get("classify").toString().split(","));
+        }
+
+        if(!StringUtils.isEmpty(params.get("build_material"))){
+            buildMaterial = Arrays.asList(params.get("build_material").toString().split(","));
+        }
+
+        if(params.get("giftPrice")!=null){
+            giftPrice= (String) params.get("giftPrice");
+        }
+
+        if(params.get("deposit")!=null){
+            deposit= (String) params.get("deposit");
+        }
+        System.out.println(StringUtils.isEmpty(params.get("build_material")));
+        List<HouseEntity> houseEntities =  baseMapper.getOrderPage(order,pageSize,pageNo,classify,buildMaterial,giftPrice,deposit);
         PageUtils pageUtils = new PageUtils(houseEntities, houseEntities.size(), pageSize, pageNo);
-        System.out.println(pageUtils);
         return pageUtils;
     }
 
